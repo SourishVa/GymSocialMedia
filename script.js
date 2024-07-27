b41deafa45e06d7da0a3d70859a1ec59c4633dbb
Sourish Vankadari <sourishvankadari@gmail.com>
2024-07-27 19:45:58 -0400

    Basics Posts
    Adds local post creation system.

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
 function saveEmail() {
     // Get the value from the text box
     const textBoxValue = document.getElementById('email').value;
+    
+    localStorage.setItem('userEmail', textBoxValue)
+
 
     // Display the value in an alert box (or you could handle it differently)
     alert('Saved Data: ' + textBoxValue);
     window.location.href = 'app.html';
     console.log(textBoxValue)
+}
+
+window.onload = function() {
+    // Get the email from localStorage
+    const email = localStorage.getItem('userEmail');
+
+    // Check if the email exists
+    if (email) {
+        // Display the email in the box
+        document.getElementById('Profile').textContent = `${email}`;
+    } else {
+        // Handle the case where the email is not found
+        document.getElementById('Profile').textContent = 'No email found.';
+    }
+}
+
+function createBox() {
+    const name = document.getElementById("WorkoutName").value
+    console.log(name)
+    const workout = document.getElementById("Workout").value
+    console.log(workout)
+
+    if(name && workout) {
+        const newPost = document.createElement("div")
+        newPost.className = "post"
+
+        newPost.innerHTML = `
+            <h3>${name}</h3>
+            <p>${workout}</p>
+            <button>Up</button>
+            <button>Down</button>
+        `
+
+        const posted = document.getElementById("PostBoard")
+        console.log(posted)
+        posted.appendChild(newPost)
+
+        document.getElementById('WorkoutName').value = '';
+        document.getElementById('Workout').value = '';
+    } else {
+        alert('Please fill out both fields.');
+    }
 }
